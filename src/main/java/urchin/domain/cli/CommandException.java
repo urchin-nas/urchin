79288dbbae31f5d91875411c8af30bb8c3a70416
Andreas Hemmingsson <andhem@gmail.com>
2016-10-29 17:33:06 +0200

    added error code to exception

--- src/main/java/urchin/domain/cli/CommandException.java
+++ src/main/java/urchin/domain/cli/CommandException.java
@@ -3,18 +3,25 @@ package urchin.domain.cli;
 public class CommandException extends RuntimeException {
 
     private final String commandName;
+    private final Integer errorCode;
 
-    public CommandException(String commandName, String message) {
-        super(message);
+    public CommandException(String commandName, int errorCode) {
+        super("Process returned code: " + errorCode);
         this.commandName = commandName;
+        this.errorCode = errorCode;
     }
 
     public CommandException(String commandName, Exception e) {
         super(e);
         this.commandName = commandName;
+        this.errorCode = null;
     }
 
     public String getCommandName() {
         return commandName;
+    }
+
+    public Integer getErrorCode() {
+        return errorCode;
     }
 }
