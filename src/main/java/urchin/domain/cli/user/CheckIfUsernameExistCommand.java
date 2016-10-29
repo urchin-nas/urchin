b64d7068a39439a1a474f558ac748c3e9ae2c734
Andreas Hemmingsson <andhem@gmail.com>
2016-10-29 17:31:03 +0200

    added command to check if username already exist in OS

---
+++ src/main/java/urchin/domain/cli/user/CheckIfUsernameExistCommand.java
@@ -0,0 +1,45 @@
+package urchin.domain.cli.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import urchin.domain.cli.BasicCommand;
+import urchin.domain.cli.CommandException;
+
+import static java.util.Arrays.copyOf;
+
+@Component
+public class CheckIfUsernameExistCommand extends BasicCommand {
+
+    private static final String USERNAME = "%username%";
+
+    private static final String[] COMMAND = new String[]{
+            "getent",
+            "passwd",
+            USERNAME,
+    };
+
+    @Autowired
+    public CheckIfUsernameExistCommand(Runtime runtime) {
+        super(runtime);
+    }
+
+    public boolean execute(String username) {
+        LOG.debug("Creating user {}", username);
+        try {
+            executeCommand(setupCommand(username));
+        } catch (CommandException e) {
+            if (e.getErrorCode() == 2) {
+                return false;
+            } else {
+                throw e;
+            }
+        }
+        return true;
+    }
+
+    private String[] setupCommand(String username) {
+        String[] command = copyOf(COMMAND, COMMAND.length);
+        command[2] = username;
+        return command;
+    }
+}
