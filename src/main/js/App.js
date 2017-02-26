0a1d6335d41ed2b8099d922f42baf5460cdad6cc
Andreas Hemmingsson <andhem@gmail.com>
2017-02-26 16:26:02 +0100

    moved logic

--- src/main/js/App.js
+++ src/main/js/App.js
@@ -1,26 +1,11 @@
 import React, {Component} from "react";
-import rest from "rest";
 import Groups from "./Groups";
 
 class App extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {groups: []};
-    }
-
-    componentDidMount() {
-        rest({
-            method: 'GET',
-            path: '/api/groups'
-        }).then(response => {
-            this.setState({groups: JSON.parse(response.entity).data});
-        });
-    }
-
     render() {
         return (
-            <Groups groups={this.state.groups}/>
+            <Groups/>
         );
     }
 }
--- src/main/js/Groups.js
+++ src/main/js/Groups.js
@@ -1,9 +1,24 @@
 import React, {Component} from "react";
+import rest from "rest";
 
 class Groups extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {groups: []};
+    }
+
+    componentDidMount() {
+        rest({
+            method: 'GET',
+            path: '/api/groups'
+        }).then(response => {
+            this.setState({groups: JSON.parse(response.entity).data});
+        });
+    }
+
     render() {
-        var groupList = this.props.groups.map(group =>
+        var groupList = this.state.groups.map(group =>
             <li key={group.groupId}>
                 {group.name}
             </li>
