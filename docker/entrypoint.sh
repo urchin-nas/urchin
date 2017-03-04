de827aea35542a3ec8b7a3b74a50cc4f67449358
Andreas Hemmingsson <andhem@gmail.com>
2017-03-04 22:53:10 +0100

    exit if tests fail

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /workspace
 
-echo "[Starting Samba]"
+echo "[Starting Samba in preparation for tests]"
 service samba start
 
 echo "[Updating repository]"
@@ -9,6 +9,10 @@ git pull
 
 echo "[Building application]"
 mvn clean install
+if [ $? -ne 0 ]; then
+    echo "failed to build and test application, exiting..."
+    exit 1
+fi
 
-echo "[Running application]"
+echo "[Starting application]"
 java -jar target/urchin-1.0-SNAPSHOT.jar
