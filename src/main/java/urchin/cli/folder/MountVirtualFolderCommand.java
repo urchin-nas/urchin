e62f2bf99d39e525767c654f40fe7a79fea4af57
Andreas Hemmingsson <andhem@gmail.com>
2017-11-05 15:06:01 +0100

    fixed typo

--- src/main/java/urchin/cli/folder/MountVirtualFolderCommand.java
+++ src/main/java/urchin/cli/folder/MountVirtualFolderCommand.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Component;
 import urchin.cli.Command;
 import urchin.cli.common.BasicCommand;
 import urchin.model.folder.Folder;
-import urchin.model.folder.UrchinFolder;
 import urchin.model.folder.VirtualFolder;
 
 import java.util.List;
@@ -31,7 +30,7 @@ public class MountVirtualFolderCommand extends BasicCommand {
     public void execute(List<Folder> folders, VirtualFolder virtualFolder) {
         LOG.debug("Mounting virtual folder {} for {} folders", virtualFolder.toAbsolutePath(), folders.size());
         List<String> folderPaths = folders.stream()
-                .map(UrchinFolder::toAbsolutePath)
+                .map(Folder::toAbsolutePath)
                 .collect(Collectors.toList());
         executeCommand(command.getFolderCommand(MOUNT_VIRTUAL_FOLDER)
                 .replace(FOLDER_LIST, arrayToDelimitedString(folderPaths.toArray(), ","))
