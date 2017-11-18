7c8c7a565fa3887305eb246eb88c4c4a3f7dc691
Andreas Hemmingsson <andhem@gmail.com>
2017-11-19 00:02:46 +0100

    changed component scan to actual class instead of package string

--- src/test/java/urchin/testutil/CliTestConfiguration.java
+++ src/test/java/urchin/testutil/CliTestConfiguration.java
@@ -3,11 +3,12 @@ package urchin.testutil;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
+import urchin.cli.BasicCommand;
 import urchin.configuration.CommandConfiguration;
 import urchin.configuration.RuntimeConfiguration;
 
 @TestConfiguration
-@ComponentScan(basePackages = "urchin.cli")
+@ComponentScan(basePackageClasses = {BasicCommand.class})
 @Import({RuntimeConfiguration.class, CommandConfiguration.class, UnixUserAndGroupCleanup.class})
 public class CliTestConfiguration {
 }
