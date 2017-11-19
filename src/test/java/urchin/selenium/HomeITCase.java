ee8193520a9ff0c379c7897c2228750a49f2910b
Andreas Hemmingsson <andhem@gmail.com>
2017-11-19 16:12:15 +0100

    Splitting ordinary integration tests and selenium tests into separate executions

--- src/test/java/urchin/selenium/HomeITCase.java
+++ src/test/java/urchin/selenium/HomeITCase.java
@@ -3,7 +3,7 @@ package urchin.selenium;
 import org.junit.Test;
 import org.openqa.selenium.By;
 
-public class HomeIT extends SeleniumTestApplication {
+public class HomeITCase extends SeleniumTestApplication {
 
     @Test
     public void homeIsDisplayedAsFirstPageWhenLoggedIn() {
