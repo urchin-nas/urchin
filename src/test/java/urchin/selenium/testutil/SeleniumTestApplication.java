628007e7eb9972f5f3526b951c27046645941311
Andreas Hemmingsson <andhem@gmail.com>
2017-11-26 12:15:32 +0100

    removed cleanup as it causes errors in backend

--- src/test/java/urchin/selenium/testutil/SeleniumTestApplication.java
+++ src/test/java/urchin/selenium/testutil/SeleniumTestApplication.java
@@ -1,12 +1,7 @@
 package urchin.selenium.testutil;
 
-import org.junit.Rule;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Import;
 import urchin.selenium.view.HomeView;
 import urchin.selenium.view.MenuView;
 import urchin.selenium.view.folders.FoldersView;
@@ -18,52 +13,23 @@ import urchin.selenium.view.groups.group.NewGroupView;
 import urchin.selenium.view.users.UsersView;
 import urchin.selenium.view.users.user.EditUserView;
 import urchin.selenium.view.users.user.NewUserView;
-import urchin.testutil.CliTestConfiguration;
-import urchin.testutil.UnixUserAndGroupCleanup;
 
 @RunWith(SeleniumRunner.class)
-@Import(CliTestConfiguration.class)
 public abstract class SeleniumTestApplication {
-
-    private static Logger log = LoggerFactory.getLogger(SeleniumTestApplication.class);
 
     protected static final WebDriver driver = SeleniumDriver.getDriver();
     protected static final String url = SeleniumUrl.getUrl();
 
-    @Rule
-    @Autowired
-    public UnixUserAndGroupCleanup unixUserAndGroupCleanup;
+    protected static HomeView homeView = new HomeView();
+    protected static MenuView menuView = new MenuView();
+    protected static GroupsView groupsView = new GroupsView();
+    protected static NewGroupView newGroupView = new NewGroupView();
+    protected static EditGroupView editGroupView = new EditGroupView();
+    protected static UsersView usersView = new UsersView();
+    protected static NewUserView newUserView = new NewUserView();
+    protected static EditUserView editUserView = new EditUserView();
+    protected static FoldersView foldersView = new FoldersView();
+    protected static NewFolderView newFolderView = new NewFolderView();
+    protected static EditFolderView editFolderView = new EditFolderView();
 
-    private static boolean initialized;
-
-    protected static HomeView homeView;
-    protected static MenuView menuView;
-    protected static GroupsView groupsView;
-    protected static NewGroupView newGroupView;
-    protected static EditGroupView editGroupView;
-    protected static UsersView usersView;
-    protected static NewUserView newUserView;
-    protected static EditUserView editUserView;
-    protected static FoldersView foldersView;
-    protected static NewFolderView newFolderView;
-    protected static EditFolderView editFolderView;
-
-    public SeleniumTestApplication() {
-        if (!initialized) {
-            log.info("Setting up PageViews");
-            homeView = new HomeView();
-            menuView = new MenuView();
-            groupsView = new GroupsView();
-            newGroupView = new NewGroupView();
-            editGroupView = new EditGroupView();
-            usersView = new UsersView();
-            newUserView = new NewUserView();
-            editUserView = new EditUserView();
-            foldersView = new FoldersView();
-            newFolderView = new NewFolderView();
-            editFolderView = new EditFolderView();
-
-            initialized = true;
-        }
-    }
 }
