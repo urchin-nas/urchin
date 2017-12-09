3752cd33839267142cacfef06d24c19f8e6b59f3
Andreas Hemmingsson <andhem@gmail.com>
2017-12-09 17:36:46 +0100

    fixed trigger of redux state update and fixed typo

--- src/main/app/src/component/navigation/Navigation.js
+++ src/main/app/src/component/navigation/Navigation.js
@@ -7,6 +7,12 @@ class Navigation extends Component {
         this.props.callbacks.toggleNavigation(this.props.navigationVisible)
     };
 
+    hideNavigation = () => {
+        if (this.props.navigationVisible) {
+            this.props.callbacks.toggleNavigation(this.props.navigationVisible)
+        }
+    };
+
     render() {
         let toggleNav = this.props.navigationVisible ? 'navigation-menu--show' : 'navigation-menu--hide';
 
@@ -18,17 +24,17 @@ class Navigation extends Component {
                 </button>
                 <div className={toggleNav} id="navigation-menu">
                     <ul id="navigation-menu__items">
-                        <il className="navigation-item">
-                            <Link id="navigation-item__users" to="/users" onClick={this.toggleNavigation}>Users</Link>
-                        </il>
-                        <il className="navigation-item">
+                        <li className="navigation-item">
+                            <Link id="navigation-item__users" to="/users" onClick={this.hideNavigation}>Users</Link>
+                        </li>
+                        <li className="navigation-item">
                             <Link id="navigation-item__groups" to="/groups"
-                                  onClick={this.toggleNavigation}>Groups</Link>
-                        </il>
-                        <il className="navigation-item">
+                                  onClick={this.hideNavigation}>Groups</Link>
+                        </li>
+                        <li className="navigation-item">
                             <Link id="navigation-item__folders" to="/folders"
-                                  onClick={this.toggleNavigation}>Folders</Link>
-                        </il>
+                                  onClick={this.hideNavigation}>Folders</Link>
+                        </li>
                     </ul>
                 </div>
 
