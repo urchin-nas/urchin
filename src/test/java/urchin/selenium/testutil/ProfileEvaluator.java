c1b193a221941ad3a27c7af51656045a8c91ab55
Andreas Hemmingsson <andhem@gmail.com>
2018-02-07 20:05:07 +0100

    changed profile for selenium

--- src/test/java/urchin/selenium/testutil/ProfileEvaluator.java
+++ src/test/java/urchin/selenium/testutil/ProfileEvaluator.java
@@ -1,13 +1,19 @@
 package urchin.selenium.testutil;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Arrays;
 
 class ProfileEvaluator {
 
-    static final String PROFILE = "production";
+    static final String PROFILE = "selenium";
+    private static final Logger log = LoggerFactory.getLogger(ProfileEvaluator.class.getName());
+
 
     static boolean isProduction() {
         String activeProfiles = System.getProperty("spring.profiles.active");
+        log.info("Active profiles: {}", activeProfiles);
         return activeProfiles != null && Arrays.asList(activeProfiles.split(",")).contains(PROFILE);
     }
 }
