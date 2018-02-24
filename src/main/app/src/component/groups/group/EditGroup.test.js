88d94512d9c9727405e724877e0e2ac13cfc25bc
Andreas Hemmingsson <andhem@gmail.com>
2018-02-24 11:54:16 +0100

    added some tests that triggers js functions

--- src/main/app/src/component/groups/group/EditGroup.test.js
+++ src/main/app/src/component/groups/group/EditGroup.test.js
@@ -4,14 +4,68 @@ import toJson from 'enzyme-to-json';
 import EditGroup from "./EditGroup";
 
 describe('EditGroup', () => {
+    let groupId = 10;
+    let userId_1 = 100;
+    let userId_2 = 101;
+
+    let callbacks = {
+        setGroup: jest.fn(),
+        deleteGroup: jest.fn(),
+        addUser: jest.fn(),
+        removeUser: jest.fn(),
+    };
 
     let props = {
-        group: {},
-        usersInGroup: [],
-        availableUsers: []
+        group: {
+            groupId: groupId,
+            userId: userId_1
+        },
+        usersInGroup: [{
+            userId: userId_2,
+            username: 'user2'
+        }],
+        availableUsers: [{
+            userId: userId_1,
+            username: 'user1'
+        }],
+        callbacks: callbacks
     };
+
+    beforeEach(() => {
+        Object.entries(callbacks).forEach(([name, fn]) => {
+            fn.mockClear();
+        });
+    });
 
     it('match snapshot', () => {
         expect(toJson(shallow(<EditGroup {...props}/>))).toMatchSnapshot();
+    });
+
+    it('deleteGroup is called when clicking delete', () => {
+        let component = shallow(<EditGroup {...props}/>);
+        component.find('[data-view="delete"]').simulate('click');
+
+        expect(callbacks.deleteGroup).toHaveBeenCalledWith(props.group.groupId)
+    });
+
+    it('setGroup is called when availableUsers select changes', () => {
+        let component = shallow(<EditGroup {...props}/>);
+        component.find('[data-view="availableUsers"]').simulate('change', {target: {value: userId_1}});
+
+        expect(callbacks.setGroup).toHaveBeenCalledTimes(1);
+    });
+
+    it('addUser is called when clicking addUser', () => {
+        let component = shallow(<EditGroup {...props}/>);
+        component.find('[data-view="addUser"]').simulate('click');
+
+        expect(callbacks.addUser).toHaveBeenCalledWith(groupId, userId_1)
+    });
+
+    it('removeUser is called when clicking removeUser', () => {
+        let component = shallow(<EditGroup {...props}/>);
+        component.find('[data-view="removeUser"]').simulate('click');
+
+        expect(callbacks.removeUser).toHaveBeenCalledWith(groupId, userId_2)
     });
 });
--- src/main/app/src/component/groups/group/NewGroup.test.js
+++ src/main/app/src/component/groups/group/NewGroup.test.js
@@ -5,11 +5,37 @@ import NewGroup from "./NewGroup";
 
 describe('NewGroup', () => {
 
+    let callbacks = {
+        setGroup: jest.fn(),
+        createGroup: jest.fn(),
+    };
+
     let props = {
         group: {},
+        callbacks: callbacks
     };
+
+    beforeEach(() => {
+        Object.entries(callbacks).forEach(([name, fn]) => {
+            fn.mockClear();
+        });
+    });
 
     it('match snapshot', () => {
         expect(toJson(shallow(<NewGroup {...props}/>))).toMatchSnapshot();
+    });
+
+    it('setGroup is called when groupName changes', () => {
+        let component = shallow(<NewGroup {...props}/>);
+        component.find('[name="groupName"]').simulate('change', {target: {value: "name"}});
+
+        expect(callbacks.setGroup).toHaveBeenCalledTimes(1);
+    });
+
+    it('createGroup is called when clicking create', () => {
+        let component = shallow(<NewGroup {...props}/>);
+        component.find('[data-view="create"]').simulate('click');
+
+        expect(callbacks.createGroup).toHaveBeenCalledWith(props.group)
     });
 });
