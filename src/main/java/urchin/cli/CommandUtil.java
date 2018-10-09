8d075c0c08af4fe86a060169758630e1b211a44c
Andreas Hemmingsson <andhem@gmail.com>
2018-10-09 21:42:51 +0200

    defaultCharset

--- src/main/java/urchin/cli/CommandUtil.java
+++ src/main/java/urchin/cli/CommandUtil.java
@@ -3,12 +3,13 @@ package urchin.cli;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.nio.charset.Charset;
 import java.util.stream.Collectors;
 
 public class CommandUtil {
 
     public static String readResponse(InputStream inputStream) {
-        return new BufferedReader(new InputStreamReader(inputStream))
+        return new BufferedReader(new InputStreamReader(inputStream, Charset.defaultCharset()))
                 .lines()
                 .collect(Collectors.joining());
     }
